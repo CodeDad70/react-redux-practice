57157bf935bfde1533ab8847c77954948bd598f0
CodeDad <github email address>
2018-08-07 15:24:23 -0400

    created handleSubmit in Form.js as well as inputs

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react';
 
+let id = 0
+
 class Form extends Component {
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let title = this.getTitle.Value;
+    let message = this.getMessage.value;
+    const data = {
+      id: new Date(),
+      title, 
+      message
+    }
+  }
+
   render() {
     return (
       
      <div>
-       <form>
+       <h2>Create Post</h2>
+       
+       <form onSubmit={this.handleSubmit}>
          
-         <input required type="text" placeholder="Enter Post Title"/><br/><br/>
+         <input 
+         required type="text" 
+         ref={(input)=>this.getTitle=input}
+         placeholder="Enter Post Title"/>
+         <br/><br/>
          
-         <textarea required rows="10" cols="40"
-         placeholder="Enter Post"/> <br/><br/>
+         <textarea 
+         required rows="10" 
+         cols="40"
+         ref={(input)=>this.getMessage=input} 
+         placeholder="Enter Post"/> 
+         <br/><br/>
          
+         <button>Post</button>
          
       </form>
      </div> 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+import App from './App';
+import Reducer from './reducers/Reducer';
 
-const store = createStore();
+  const store = createStore(Reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(
+    <Provider store ={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
 
